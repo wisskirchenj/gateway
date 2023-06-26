56ed4b3dd35353e0eef3b9db47fdffca5f9fa931
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-06-26 17:47:38 +0200

    Initial setup

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.gradle.internal.classpath.Instrumented
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.1"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.graalvm.buildtools.native") version "0.9.23"
+}
+
+group = "de.cofinpro"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    //implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.named<BootBuildImage>("bootBuildImage") {
+    Instrumented.systemProperty("spring.profiles.active", "k8s")
+    builder.set("dashaun/builder:tiny")
+    environment.put("BP_NATIVE_IMAGE", "true")
+}
---
+++ src/main/java/de/cofinpro/register/RegisterServerApplication.java
@@ -0,0 +1,13 @@
+package de.cofinpro.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterServerApplication.class, args);
+    }
+
+}
