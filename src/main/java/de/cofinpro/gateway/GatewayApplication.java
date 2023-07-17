040a9a7fcddcba36341b06845a4c6ef86a9afaca
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-07-17 21:28:20 +0200

    Add proxy registration for Hibernate 6.2.5 native

--- src/main/java/de/cofinpro/gateway/GatewayApplication.java
+++ src/main/java/de/cofinpro/gateway/GatewayApplication.java
@@ -1,11 +1,14 @@
 package de.cofinpro.gateway;
 
+import de.cofinpro.gateway.aot.HibernateProxyRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@ImportRuntimeHints({HibernateProxyRegistrar.class})
 public class GatewayApplication {
 
     public static void main(String[] args) {
