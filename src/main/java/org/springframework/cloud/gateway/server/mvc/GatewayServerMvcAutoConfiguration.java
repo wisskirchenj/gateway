06826a0276ea4061774705cdf996ee2fe952c2a8
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-12-24 23:50:04 +0100

    http-client setup and runtime hints

--- src/main/java/org/springframework/cloud/gateway/server/mvc/GatewayServerMvcAutoConfiguration.java
+++ src/main/java/org/springframework/cloud/gateway/server/mvc/GatewayServerMvcAutoConfiguration.java
@@ -16,7 +16,6 @@
 
 package org.springframework.cloud.gateway.server.mvc;
 
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -29,11 +28,9 @@ import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.cloud.gateway.server.mvc.common.ArgumentSupplierBeanPostProcessor;
-import org.springframework.cloud.gateway.server.mvc.config.FilterProperties;
+import org.springframework.cloud.gateway.server.mvc.config.GatewayMvcAotRuntimeHintsRegistrar;
 import org.springframework.cloud.gateway.server.mvc.config.GatewayMvcProperties;
 import org.springframework.cloud.gateway.server.mvc.config.GatewayMvcPropertiesBeanDefinitionRegistrar;
-import org.springframework.cloud.gateway.server.mvc.config.PredicateProperties;
-import org.springframework.cloud.gateway.server.mvc.config.RouteProperties;
 import org.springframework.cloud.gateway.server.mvc.config.RouterFunctionHolderFactory;
 import org.springframework.cloud.gateway.server.mvc.filter.FormFilter;
 import org.springframework.cloud.gateway.server.mvc.filter.ForwardedRequestHeadersFilter;
@@ -52,6 +49,7 @@ import org.springframework.cloud.gateway.server.mvc.predicate.PredicateDiscovere
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.core.env.Environment;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
@@ -60,8 +58,8 @@ import org.springframework.web.client.RestClient;
 
 @AutoConfiguration(after = {RestTemplateAutoConfiguration.class, RestClientAutoConfiguration.class})
 @ConditionalOnProperty(name = "spring.cloud.gateway.mvc.enabled", matchIfMissing = true)
-@Import({GatewayMvcPropertiesBeanDefinitionRegistrar.class})
-@RegisterReflectionForBinding({FilterProperties.class, PredicateProperties.class, RouteProperties.class})
+@Import(GatewayMvcPropertiesBeanDefinitionRegistrar.class)
+@ImportRuntimeHints(GatewayMvcAotRuntimeHintsRegistrar.class)
 public class GatewayServerMvcAutoConfiguration {
 
     @Bean
