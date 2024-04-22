41912b993553da75e06f26631bfd3ceaf99f7d87
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2024-04-22 14:50:22 +0200

    bump actions remove cloud workaround

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,17 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     java
-    id("org.springframework.boot") version "3.2.0"
+    id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
-    id("org.graalvm.buildtools.native") version "0.9.28"
+    id("org.graalvm.buildtools.native") version "0.10.1"
 }
-val springCloudVersion = "2023.0.0"
-val springCloudGatewayVersion = "4.1.2-SNAPSHOT"
-/*
+val springCloudVersion = "2023.0.1"
+
 dependencyManagement {
     imports {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
     }
-}*/
+}
 
 group = "de.cofinpro"
 version = "0.1.4-SNAPSHOT"
@@ -38,8 +37,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc:${springCloudGatewayVersion}")
-    implementation("org.springframework.cloud:spring-cloud-gateway:${springCloudGatewayVersion}")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
 
     compileOnly("org.projectlombok:lombok")
     //developmentOnly("org.springframework.boot:spring-boot-devtools")
